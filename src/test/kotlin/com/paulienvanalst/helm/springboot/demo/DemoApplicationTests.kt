e3fad3b7d7c434ac823453d8c1a1e69384658338
paulienvanalst <paulienvanalst@abc.nl>
2024-04-11 12:52:54 +0200

    Initial commit

---
+++ src/test/kotlin/com/paulienvanalst/helm/springboot/demo/DemoApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.paulienvanalst.helm.springboot.demo
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class DemoApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
