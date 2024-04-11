851ed318c5695b7b671be55f4c8bb1936b4c4e37
paulienvanalst <paulienvanalst@abc.nl>
2024-04-11 13:21:18 +0200

    Remove references

--- src/test/kotlin/com/paulienvanalst/helm/springboot/demo/TestDemoApplication.kt
+++ src/test/kotlin/com/paulienvanalst/helm/springboot/demo/TestDemoApplication.kt
@@ -5,8 +5,8 @@ import org.springframework.boot.test.context.TestConfiguration
 import org.springframework.boot.testcontainers.service.connection.ServiceConnection
 import org.springframework.boot.with
 import org.springframework.context.annotation.Bean
-import org.testcontainers.containers.PostgreSQLContainer
-import org.testcontainers.utility.DockerImageName
+# import org.testcontainers.containers.PostgreSQLContainer
+# import org.testcontainers.utility.DockerImageName
 
 @TestConfiguration(proxyBeanMethods = false)
 class TestDemoApplication {
